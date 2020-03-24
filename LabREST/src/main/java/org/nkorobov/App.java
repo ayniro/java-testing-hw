3fa8d3b995db529b438acbbf046076ef5194acdb
Nick Korobov <nickkorobov3d@gmail.com>
2020-03-24 13:59:45 +0300

    LabREST Initial commit

---
+++ LabREST/src/main/java/org/nkorobov/App.java
@@ -0,0 +1,13 @@
+package org.nkorobov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

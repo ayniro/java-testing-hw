e36089d5d26bad44c0234ecf0510c67a60b4b8d5
Nick Korobov <nickkorobov3d@gmail.com>
2020-03-25 07:50:14 +0300

    LabREST -- Added API tests

---
+++ LabREST/src/test/java/org/nkorobov/AlphaVantageApiTests.java
@@ -0,0 +1,67 @@
+package org.nkorobov;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class AlphaVantageApiTests {
+    private static final String url = "https://www.alphavantage.co";
+    private static final String apiKey = /*to be filled*/;
+
+    @BeforeClass
+    public static void setupRestAssured() {
+        RestAssured.requestSpecification = new RequestSpecBuilder().
+                setBaseUri(url + "/query").
+                setAccept(ContentType.JSON).
+                addQueryParam("apikey", apiKey).build();
+        RestAssured.responseSpecification = new ResponseSpecBuilder().
+                expectStatusCode(200).build();
+    }
+
+    @Test
+    public void getGlobalQuoteTest()
+    {
+        Response response =
+                given().
+                    param("function", "GLOBAL_QUOTE").
+                    param("symbol", "SPY").
+                when().
+                    get().
+                then().
+                    body(matchesJsonSchemaInClasspath("globalQuoteJsonSchema.json")).
+                    extract().response();
+
+        JsonPath path = new JsonPath(response.asString());
+        Assert.assertEquals("SPY", path.getString("'Global Quote'.'01. symbol'"));
+    }
+
+    @Test
+    public void symbolSearchTest() {
+        Response response =
+                given().
+                    param("function", "SYMBOL_SEARCH").
+                    param("keywords", "tesla").
+                    param("datatype", "json").
+                when().
+                    get().
+                then().
+                    body(matchesJsonSchemaInClasspath("symbolSearchJsonSchema.json")).
+                extract().response();
+
+        JsonPath path = new JsonPath(response.asString());
+        Assert.assertEquals("TSLA", path.getString("bestMatches[0].'1. symbol'"));
+        Assert.assertEquals("Tesla Inc.", path.getString("bestMatches[0].'2. name'"));
+        Assert.assertEquals("Equity", path.getString("bestMatches[0].'3. type'"));
+        Assert.assertEquals("United States", path.getString("bestMatches[0].'4. region'"));
+        Assert.assertEquals("USD", path.getString("bestMatches[0].'8. currency'"));
+    }
+}
